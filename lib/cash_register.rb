5e2ead396173915cc4d30c6fa64b23988739af26
danny galimi <dannygalimi13@aim.com>
2020-09-30 19:39:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :total ,:discount
+  
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+  end
+    
+  def add_item(title,price,quantity = 1)
+    self.total = price * quantity +self.total
+    self.total 
+    end
+    
+    def apply_discount
+    binding.pry
+    real_discount = self.discount / 100
+    
+    savings = self.total * real_discount 
+    self.total = self.total - savings
+    self.total
+    end 
+    
+end
